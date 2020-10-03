5cd2b0ff2ffa3288daf62aaf0126f86f5bfb40b9
Uvarova-Viktoria <59088574+Uvarova-Viktoria@users.noreply.github.com>
2020-10-03 14:29:51 +0300

    init: start typical-array-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array==undefined || array.length==0){
+    return 0;
+  }
+  else{
+      return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array==undefined || array.length==0){
+    return 0;
+  }
+  else{
+      return Math.max(...array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  
+  if(array==undefined || array.length==0){
+    return 0;
+  }
+  else{
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return (array.reduce(reducer))/(array.length);
+  }
 }
